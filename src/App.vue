f06b317cc6ccd0c894be5709c3a548f3ede7afaa
Александра <alexsandra.mayer@yandex.ru>
2023-10-19 22:26:45 +0500

    hw5

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<IndexPage/>
+	<BlogPage/>
+	<BlogDetailsPage/>
+</template>
+
+<script>
+import IndexPage from './components/IndexPage.vue';
+import BlogPage from './components/BlogPage.vue';
+import BlogDetailsPage from './components/BlogDetailsPage.vue';
+
+export default { 
+	components: {
+		IndexPage,
+		BlogPage,
+		BlogDetailsPage
+	},
+};
+</script>
+
+<style lang="scss">
+</style>
